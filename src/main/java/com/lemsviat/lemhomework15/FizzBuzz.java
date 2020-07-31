dcb2efece2691e3a577ff3ab397a6dd5e962bf1b
Sviatoslav Lementar <lemsviat@gmail.com>
2020-07-31 22:31:57 +0300

    Second commit

--- src/main/java/com/lemsviat/lemhomework15/FizzBuzz.java
+++ src/main/java/com/lemsviat/lemhomework15/FizzBuzz.java
@@ -1,33 +1,71 @@
 package main.java.com.lemsviat.lemhomework15;
 
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class FizzBuzz implements Runnable {
-    int number;
+    private Semaphore sem1, sem2, sem3, sem4;
 
     public FizzBuzz() {
+        this.sem1 = new Semaphore(1, true);
+        this.sem2 = new Semaphore(0, true);
+        this.sem3 = new Semaphore(0, true);
+        this.sem4 = new Semaphore(0, true);
     }
 
+    public String fizz(int m) {
+        String s = "";
+        try {
+            sem1.acquire();
+            if (m % 3 == 0 && !(m % 5 == 0)) s = "fizz, ";
+            //System.out.println("s = " + s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sem2.release();
+        return s;
 
-    public void fizz(int m) {
-        if (m % 3 == 0 && !(m % 5 == 0)) System.out.println("fizz, ");
     }
 
-    public void buzz(int m) {
-        if (m % 5 == 0 && !(m % 3 == 0)) System.out.println("buzz, ");
+    public String buzz(int m) {
+        String s = "";
+        try {
+            sem2.acquire();
+            if (m % 5 == 0 && !(m % 3 == 0)) s = "buzz, ";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sem3.release();
+        return s;
     }
 
-    public void fizzbuzz(int m) {
-        if (m % 3 == 0 && m % 5 == 0) System.out.println("fizzbuzz, ");
+    public String fizzbuzz(int m) {
+        String s = "";
+        try {
+            sem3.acquire();
+            if (m % 3 == 0 && m % 5 == 0) s = "fizzbuzz, ";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sem4.release();
+        return s;
     }
 
-    public void number(int m) {
-        if (!(m % 5 == 0) && !(m % 3 == 0)) System.out.println(m + ", ");
+    public String number(int m) {
+        String s="";
+        try {
+            sem4.acquire();
+            if (!(m % 5 == 0) && !(m % 3 == 0)) s = m + ", ";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sem1.release();
+        return s;
     }
 
     @Override
     public void run() {
-        int n = 9;
+        int n = 1;
         fizz(n);
         buzz(n);
         fizzbuzz(n);
--- src/main/java/com/lemsviat/lemhomework15/NumberToString.java
+++ src/main/java/com/lemsviat/lemhomework15/NumberToString.java
@@ -1,36 +1,59 @@
 package main.java.com.lemsviat.lemhomework15;
 
+import java.io.StringBufferInputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
+
 
 public class NumberToString {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
         int n = 15;
         ArrayList<Integer> list = new ArrayList<>(n);
         for (int i = 1; i <= n; i++) {
             list.add(i);
         }
         FizzBuzz f = new FizzBuzz();
-        System.out.println("исходный массив");
-        for (int item:list
-             ){
-            System.out.print(item+" ");
+        System.out.println("Исходный массив:");
+        for (int item : list
+        ) {
+            sb.append(item).append(", ");
         }
-        System.out.println();
-        int i=14;
-        int k=list.get(i);
-        Thread myThreadA = new Thread(() -> f.fizz(k));
-        Thread myThreadB = new Thread(() -> f.buzz(k));
-        Thread myThreadC = new Thread(() -> f.fizzbuzz(k));
-        Thread myThreadD = new Thread(() -> f.number(k));
-        //do {
-            myThreadA.start();
-            myThreadB.start();
-            myThreadC.start();
-            myThreadD.start();
-            i++;
+        sb.replace(sb.length() - 2,sb.length()-1,".");
+        System.out.print(sb+"\n");
 
-       // } while (i<n);
+        System.out.println("Преобразованный массив:");
+        Thread myThreadA = new Thread(() -> {
+            for (int item : list
+            ) {
+                sb1.append(f.fizz(item));
+            }
+        });
+        Thread myThreadB = new Thread(() -> {
+            for (int item : list
+            ) {
+                sb1.append(f.buzz(item));
+            }
+        });
+        Thread myThreadC = new Thread(() -> {
+            for (int item : list
+            ) {
+                sb1.append(f.fizzbuzz(item));
+            }
+        });
+        Thread myThreadD = new Thread(() -> {
+            for (int item : list
+            ) {
+                sb1.append(f.number(item));
+            }
+        });
 
+        myThreadA.start();
+        myThreadB.start();
+        myThreadC.start();
+        myThreadD.start();
+
+        //sb1.replace(sb1.length() - 1,sb1.length(),".");
+        System.out.print(sb1+"\n");
     }
 }
