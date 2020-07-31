6a1ab3319493cfecbace85654f316311d454bd46
Sviatoslav Lementar <lemsviat@gmail.com>
2020-07-31 23:28:37 +0300

    Third commit

--- src/main/java/com/lemsviat/lemhomework15/NumberToString.java
+++ src/main/java/com/lemsviat/lemhomework15/NumberToString.java
@@ -1,28 +1,26 @@
 package main.java.com.lemsviat.lemhomework15;
-
-import java.io.StringBufferInputStream;
 import java.util.ArrayList;
-
 
 public class NumberToString {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
-        StringBuilder sb1 = new StringBuilder();
+        StringBuffer sb1 = new StringBuffer();
         int n = 15;
         ArrayList<Integer> list = new ArrayList<>(n);
         for (int i = 1; i <= n; i++) {
             list.add(i);
         }
         FizzBuzz f = new FizzBuzz();
+
         System.out.println("Исходный массив:");
         for (int item : list
         ) {
             sb.append(item).append(", ");
         }
-        sb.replace(sb.length() - 2,sb.length()-1,".");
-        System.out.print(sb+"\n");
+        sb.replace(sb.length() - 2, sb.length() - 1, ".");
+        System.out.print(sb + "\n");
 
-        System.out.println("Преобразованный массив:");
+        System.out.print("Преобразованный массив:\n");
         Thread myThreadA = new Thread(() -> {
             for (int item : list
             ) {
@@ -46,14 +44,12 @@ public class NumberToString {
             ) {
                 sb1.append(f.number(item));
             }
+            sb1.replace(sb1.length() - 2,sb1.length()-1,".");
+            System.out.print(sb1);
         });
-
         myThreadA.start();
         myThreadB.start();
         myThreadC.start();
         myThreadD.start();
-
-        //sb1.replace(sb1.length() - 1,sb1.length(),".");
-        System.out.print(sb1+"\n");
     }
 }
